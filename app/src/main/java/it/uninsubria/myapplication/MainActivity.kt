23f02e1c27c95d8f092b89b61574060fc014cb6b
Stefano <thetimeste@hotmail.it>
2020-03-21 17:09:52 +0100

    Initial commit

---
+++ app/src/main/java/it/uninsubria/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package it.uninsubria.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            updateTextButton.setOnClickListener { welcomeTextView.text = "Hello Kotlin World!" }
+
+
+    }
+}
